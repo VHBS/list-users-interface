b58fa09c86b2107c6edbd3426d87da2b5cec6f3d
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2023-04-12 21:54:22 -0300

    feat: adding title and favicon

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
+import { UserContextProvider } from '@component/context/context'
 import '@component/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+  <>
+    <UserContextProvider>
+      <Head>
+        <title>Users Web Interface Project</title>
+      </Head>
+      <Component {...pageProps} />
+    </UserContextProvider>
+  </>
+  )
 }
