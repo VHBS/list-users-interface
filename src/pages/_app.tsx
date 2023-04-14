bc5e6aa8182d6e726aa015bb3322fcd4311e676c
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2023-04-13 22:54:08 -0300

    feat: ajusting eslint rules to accept a jsx global style (like nextjs docs)

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,25 @@
-import { UserContextProvider } from '@component/context/context'
-import '@component/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from 'next/head'
+import { UserContextProvider } from '@component/context/Context';
+import '@component/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Head from 'next/head';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-  <>
-    <UserContextProvider>
-      <Head>
-        <title>Users Web Interface Project</title>
-      </Head>
-      <Component {...pageProps} />
-    </UserContextProvider>
-  </>
-  )
+    <>
+      <style jsx global>{`
+        html {
+          font-family: ${inter.style.fontFamily};
+        }
+      `}</style>
+      <UserContextProvider>
+        <Head>
+          <title>Users Web Interface Project</title>
+        </Head>
+        <Component {...pageProps} />
+      </UserContextProvider>
+    </>
+  );
 }
