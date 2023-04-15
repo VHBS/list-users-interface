6c4f12cf92cd99112bb4dfa29bee10f850c1353f
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2023-04-15 14:24:49 -0300

    fix: changing link leaflet to document file

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,12 @@ export default function Document() {
     <Html lang="pt-BR">
       <Head>
         <link rel="icon" href="/favicon.ico"></link>
+        <link
+          rel="stylesheet"
+          href="https://unpkg.com/leaflet@1.9.3/dist/leaflet.css"
+          integrity="sha256-kLaT2GOSpHechhsozzB+flnD+zUyjE2LlfWPgU04xyI="
+          crossOrigin=""
+        />
       </Head>
       <body>
         <Main />
