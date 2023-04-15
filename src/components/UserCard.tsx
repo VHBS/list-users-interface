f7b10b3bf5baf2bb1eabb7bd626caf868258d2a3
Victor Hugo Baum Schlichting <victorgow2@gmail.com>
2023-04-14 22:31:23 -0300

    feat: finishing style user card component

--- src/components/UserCard.tsx
+++ src/components/UserCard.tsx
@@ -1,4 +1,5 @@
 import { UserType } from '@component/@types/user';
+import { capitalize } from '@component/utils/utils';
 import { FiMail, FiPhone } from 'react-icons/fi';
 
 type UserCardProps = {
@@ -7,14 +8,22 @@ type UserCardProps = {
 
 export default function UserCard({ user }: UserCardProps) {
   return (
-    <div className="my-3 relative bg-white rounded drop-shadow card-user">
+    <div
+      className="my-3
+      relative
+      bg-white rounded
+      drop-shadow
+      card-user
+    flex
+    flex-col"
+    >
       <img
         className="w-1/2 absolute rounded-full drop-shadow-md user-picture"
         alt="United States"
         src={user.picture.large}
       />
       <div className="container container-user-country-flag rounded-t">
-        <div className="w-full gradient-picture ">
+        <div className="w-full gradient-flag ">
           <img
             className="w-full relative rounded-t user-country-flag"
             alt="United States"
@@ -23,60 +32,69 @@ export default function UserCard({ user }: UserCardProps) {
         </div>
       </div>
 
-      <div className="w-full my-3 p-3 flex flex-col justify-center">
+      <div className="w-full mt-3 py-4 px-10 flex flex-col justify-center">
         <h1 className="font-bold my-3 text-center truncate">
           {user.name.first} {user.name.last}
         </h1>
         <div className="flex justify-between  items-center text-gray-700">
-          gender:
-          <div className="flex justify-center items-center">{user.gender}</div>
+          Gender:
+          <div className="flex justify-center items-center">
+            {capitalize(user.gender)}
+          </div>
         </div>
-        <p>{user.cell}</p>
+        <div className="flex justify-between  items-center text-gray-700">
+          <div className="flex justify-center items-center">
+            {/* {user.location.city} */}
+          </div>
+        </div>
       </div>
-      <button
-        className="rounded
-          align-center
-          p-1
-          bg-neutral-300
-          text-slate-100
-          hover:bg-neutral-900
-          "
-        type="button"
-        title={user.email}
-        onClick={() => navigator.clipboard.writeText(user.email)}
-      >
-        Detalhes
-      </button>
-      <div className="w-full">
+
+      <div className="w-full flex justify-center">
         <button
           className="rounded-l
-          w-1/2
+          w-1/3
           p-2.5
-          bg-neutral-300
+          bg-stone-300
           text-slate-100
-          hover:bg-neutral-900
+          hover:bg-green-400
           "
           type="button"
-          title={user.email}
-          onClick={() => navigator.clipboard.writeText(user.email)}
+          title={user.cell}
+          onClick={() => navigator.clipboard.writeText(user.cell)}
         >
           <FiPhone className="mx-auto" />
         </button>
         <button
           className="rounded-r
-          w-1/2
+          w-1/3
           p-2.5
-          bg-neutral-300
+          bg-blue-500
           text-slate-100
-          hover:bg-neutral-900
+          hover:bg-blue-800
+          border-l
           "
           type="button"
           title={user.email}
+          // onClick={() => window.open('mailstring')}
           onClick={() => navigator.clipboard.writeText(user.email)}
         >
           <FiMail className="mx-auto" />
         </button>
       </div>
+      <button
+        className="rounded-b
+          mt-2
+          outline-1
+          p-1
+          text-purple-400
+          hover:bg-purple-600
+          hover:text-gray-100
+          "
+        type="button"
+        title="Details"
+      >
+        Details
+      </button>
     </div>
   );
 }
